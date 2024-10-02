6ceb2f257cdf33c5b2e373b0d7415427564169e4
--global <gordon.andrew.macdonald@gmail.com>
2024-10-02 17:18:37 -0600

    Add interfaces for CArcPCI CArcPCIe and ArcDefs

---
+++ ArcLib/ArcLib/ArcPCI.i
@@ -0,0 +1,90 @@
+%define ModuleDocStr
+"Python interface to CArcDeinterlace C++ code."
+%enddef
+
+%feature("autodoc", "1");
+%module(package="ArcLib", docstring=ModuleDocStr) ArcPCI
+
+%{
+
+#include <string_view>
+#include <filesystem>
+#include <vector>
+#include <string>
+#include <memory>
+
+#include <CArcDeviceDllMain.h>
+#include <CArcPCIBase.h>
+#include <CArcStringList.h>
+#include <ArcDefs.h>
+#include <CArcDevice.h>
+#include <CArcPCI.h>
+%}
+
+%init %{
+%}
+
+#define __attribute__(x)
+#define GEN3_CARCDEVICE_API __attribute__((visibility("default")))
+
+// Specifies the default C++ to python exception handling interface
+%exception {
+    try {
+        $action
+    } catch (std::domain_error & e) {
+        PyErr_SetString(PyExc_ArithmeticError, e.what());
+        SWIG_fail;
+    } catch (std::invalid_argument & e) {
+        PyErr_SetString(PyExc_ValueError, e.what());
+        SWIG_fail;
+    } catch (std::length_error & e) {
+        PyErr_SetString(PyExc_IndexError, e.what());
+        SWIG_fail;
+    } catch (std::out_of_range & e) {
+        PyErr_SetString(PyExc_ValueError, e.what());
+        SWIG_fail;
+    } catch (std::logic_error & e) {
+        PyErr_SetString(PyExc_RuntimeError, e.what());
+        SWIG_fail;
+    } catch (std::range_error & e) {
+        PyErr_SetString(PyExc_ValueError, e.what());
+        SWIG_fail;
+    } catch (std::underflow_error & e) {
+        PyErr_SetString(PyExc_ArithmeticError, e.what());
+        SWIG_fail;
+    } catch (std::overflow_error & e) {
+        PyErr_SetString(PyExc_OverflowError, e.what());
+        SWIG_fail;
+    } catch (std::runtime_error & e) {
+        PyErr_SetString(PyExc_RuntimeError, e.what());
+        SWIG_fail;
+    } catch (std::exception & e) {
+        PyErr_SetString(PyExc_Exception, e.what());
+        SWIG_fail;
+    } catch (...) {
+        SWIG_fail;
+    }
+}
+
+%include "std_unique_ptr.i"
+%include "std_shared_ptr.i"
+%include "std_string.i"
+%include "std_string_view.i"
+%include "std_filesystem.i"
+%include "std_vector.i"
+%include "typemaps.i"
+%include "stdint.i"
+
+%feature("notabstract") arc::gen3::CArcPCI;
+
+%include "CArcDevice.h"
+%import "CArcPCIBase.h"
+%include "CArcPCI.h"
+
+//%extend arcticICC::CameraConfig {
+//    std::string __repr__() const {
+//        std::ostringstream os;
+//        os << (*self);
+//        return os.str();
+//    }
+//}
--- ArcLib/setup.py
+++ ArcLib/setup.py
@@ -3,12 +3,36 @@
 from setuptools import Extension, setup
 import glob
 
-srcList = glob.glob("src/ARC_API/3.6.2/CArcDeinterlace/src/*.cpp")
-srcList.extend( glob.glob("ArcLib/*.i") )
-srcList.append( "src/ARC_API/3.6.2/CArcBase/src/CArcBase.cpp")
-srcList.append( "src/ARC_API/3.6.2/CArcBase/src/CArcStringList.cpp" )
+srcDict={}
+
+#TODO: srcList needs to be a dict.
+srcDict['ArcDeinterlace'] = glob.glob("src/ARC_API/3.6.2/CArcDeinterlace/src/*.cpp")
+srcDict['ArcDeinterlace'].append( "ArcLib/ArcDeinterlace.i")
+srcDict['ArcDeinterlace'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcBase.cpp")
+srcDict['ArcDeinterlace'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcStringList.cpp")
+srcDict['ArcFitsFile'] = glob.glob("src/ARC_API/3.6.2/CArcFitsFile/src/*.cpp")
+srcDict['ArcFitsFile'].append( "ArcLib/ArcFitsFile.i")
+srcDict['ArcFitsFile'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcStringList.cpp")
+srcDict['ArcFitsFile'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcBase.cpp")
+srcDict['ArcPCI'] = glob.glob("src/ARC_API/3.6.2/CArcDevice/src/*.cpp")
+srcDict['ArcPCI'].append( "ArcLib/ArcPCI.i")
+srcDict['ArcPCI'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcBase.cpp")
+srcDict['ArcPCI'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcStringList.cpp")
+srcDict['ArcPCIe'] = glob.glob("src/ARC_API/3.6.2/CArcDevice/src/*.cpp")
+srcDict['ArcPCIe'].append( "ArcLib/ArcPCIe.i")
+srcDict['ArcPCIe'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcBase.cpp")
+srcDict['ArcPCIe'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcStringList.cpp")
+srcDict['ArcDefs'] = ["ArcLib/ArcDefs.i"]
+srcDict['PCI'] = glob.glob("src/ARC_API/3.6.2/CArcDevice/src/*.cpp")
+srcDict['PCI'].append( "ArcLib/PCI.i" )
+srcDict['PCI'].append( "src/PCI.cpp" )
+srcDict['PCI'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcStringList.cpp" )
+srcDict['PCI'].append( "src/ARC_API/3.6.2/CArcBase/src/CArcBase.cpp")
+
 
 incList = glob.glob("src/ARC_API/3.6.2/*/inc")
+incList.append("src/ARC_API/3.6.2/cfitsio-3450/include")
+incList.append("include")
 
 swigOpts = ["-I" + i for i in incList]
 swigOpts.append("-Iinclude")
@@ -18,9 +42,56 @@ swigOpts.append("-Wall")
 setup(
         name="ArcLib",
         ext_modules=[
+#            Extension(
+#                name="_ArcDeinterlace",
+#                sources=srcDict['ArcDeinterlace'],
+#                include_dirs=incList,
+#                library_dirs=[
+#                    ],
+#                libraries=[
+#                    ],
+#                define_macros=[
+#                    ],
+#                extra_compile_args=[
+#                    "-std=c++20",
+#                    ],
+#                swig_opts=swigOpts,
+#                ),
+#            Extension(
+#                name="_ArcFitsFile",
+#                sources=srcDict['ArcFitsFile'],
+#                include_dirs=incList,
+#                library_dirs=[
+#                    "src/ARC_API/3.6.2/cfitsio-3450/lib/",
+#                    ],
+#                libraries=[
+#                    "cfitsio",
+#                    ],
+#                define_macros=[
+#                    ],
+#                extra_compile_args=[
+#                    "-std=c++20",
+#                    ],
+#                swig_opts=swigOpts,
+#                ),
+#            Extension(
+#                name="_ArcPCI",
+#                sources=srcDict['ArcPCI'],
+#                include_dirs=incList,
+#                library_dirs=[
+#                    ],
+#                libraries=[
+#                    ],
+#                define_macros=[
+#                    ],
+#                extra_compile_args=[
+#                    "-std=c++20",
+#                    ],
+#                swig_opts=swigOpts,
+#                ),
             Extension(
-                name="_ArcDeinterlace",
-                sources=srcList,
+                name="_ArcPCIe",
+                sources=srcDict['ArcPCIe'],
                 include_dirs=incList,
                 library_dirs=[
                     ],
@@ -33,6 +104,37 @@ setup(
                     ],
                 swig_opts=swigOpts,
                 ),
+#            Extension(
+#                name="_ArcDefs",
+#                sources=srcDict['ArcDefs'],
+#                include_dirs=incList,
+#                library_dirs=[
+#                    ],
+#                libraries=[
+#                    ],
+#                define_macros=[
+#                    ],
+#                extra_compile_args=[
+#                    "-std=c++20",
+#                    ],
+#                swig_opts=swigOpts,
+#                ),
+#            Extension(
+#                name="_PCI",
+#                sources=srcDict['PCI'],
+#                include_dirs=incList,
+#                library_dirs=[
+#                    ],
+#                libraries=[
+#                    ],
+#                define_macros=[
+#                    ],
+#                extra_compile_args=[
+#                    "-std=c++20",
+#                    ],
+#                swig_opts=swigOpts,
+#                ),
+
             ],
         )
 
